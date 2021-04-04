435b4487cb12088c6aabfdb48f56b6f4ae787d97
valls <olavalls@gmail.com>
2021-04-04 16:50:57 -0300

    Subindo os primeiros exercícios

--- exercises/exec-1/src/main/java/main/MainExercises.java
+++ exercises/exec-1/src/main/java/main/MainExercises.java
@@ -9,12 +9,53 @@ public class MainExercises {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double raio = sc.nextDouble();
-        System.out.printf("%.4f", calcArea(raio));
+        // exec2(sc);
+        // exec3(sc);
+        // exec4(sc);
+        exec5(sc);
     }
 
-    public static double calcArea(double raio) {
+    public static void exec2(Scanner sc) {
+        double raio = sc.nextDouble();
         double pi = 3.14159;
-        return pi * (raio * raio);
+        System.out.printf("%.4f", pi * (raio * raio));
+    }
+
+    public static void exec3(Scanner sc) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println((a*b) - (c*d));
+    }
+
+    public static void exec4(Scanner sc) {
+        int number = sc.nextInt();
+        double hours = sc.nextDouble();
+        double valuePerHour = sc.nextDouble();
+
+        System.out.println("NUMBER: " + number);
+        System.out.printf("SALARY: %.2f", (hours * valuePerHour));
+    }
+
+    public static void exec5(Scanner sc) {
+        System.out.println("ID produto 1");
+        int productId1 = sc.nextInt();
+        System.out.println("Quantidade produto 1");
+        int quantity1 = sc.nextInt();
+        System.out.println("Preçco produto 1");
+        double price1 = sc.nextDouble();
+
+        System.out.println("ID produto 2");
+        int productId2 = sc.nextInt();
+        System.out.println("Quantidade produto 2");
+        int quantity2 = sc.nextInt();
+        System.out.println("Preço produto 2");
+        double price2 = sc.nextDouble();
+
+        double result = (quantity1 * price1) + (quantity2 * price2);
+
+        System.out.printf("VALOR A PAGAR: %.2f", result);
     }
 }
