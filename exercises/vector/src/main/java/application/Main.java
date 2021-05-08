51275b07e7c691681970ed94c0f835e6335bb680
valls <olavalls@gmail.com>
2021-05-07 23:34:31 -0300

    Iniciando com calculo de media de valores de array percorrendo

--- exercises/vector/src/main/java/application/Main.java
+++ exercises/vector/src/main/java/application/Main.java
@@ -9,30 +9,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Qual o número de posições a ser armazenado");
-        int arrayPositions = sc.nextInt();
-        double[] values = new double[arrayPositions];
-        System.out.println("Digite o primeiro valor");
-        sc.nextLine();
+        int n = sc.nextInt();
+        double[] values = new double[n];
 
-        double value1 = sc.nextDouble();
-        values[0] = value1;
-        System.out.println("Digite o segundo valor");
-        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Digite o valor " + (i + 1));
+            sc.nextLine();
+            values[i] = sc.nextDouble();
+        }
 
-        double value2 = sc.nextDouble();
-        values[1] = value2;
-        System.out.println("Digite o terceiro valor");
-        sc.nextLine();
-
-        double value3 = sc.nextDouble();
-        values[2] = value3;
 
         double average = 0.0;
-        for (var i = 0; i < arrayPositions; i++) {
+        for (var i = 0; i < n; i++) {
             average += values[i];
         }
 
-        System.out.printf( "O valor da média das alturas é: %.2f", average/arrayPositions );
+        System.out.printf( "O valor da média das alturas é: %.2f", average/n );
 
         sc.close();
     }
